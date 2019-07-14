e55b213ecc0d0102ecd10c364d75a9b957a36b5b
Kicky Maulana <kickymaulana@gmail.com>
2019-07-14 11:45:30 +0700

    menambahkan fitur baru

--- jqueryku.js
+++ jqueryku.js
@@ -2,14 +2,36 @@ $(document).ready(function(){
 	const arrayku = [];
 	$('form').submit(function(e){
 		e.preventDefault();
-		arrayku.push($('[name="nama"]').val());
-		console.log(arrayku);
-		tampildata();
+		// arrayku.push($('[name="nama"]').val());
+		if ($('[name="nama"]').val() == '' || $('[name="alamat"]').val() == '' || $('[name="pekerjaan"]').val() == '') {
+			$('.notifikasi').animate({
+				'left' : '100px'
+			}).fadeOut(3000, function(){
+				$('.notifikasi').css({
+					'display' : 'block',
+					'left' : '-500px',
+					'position' : 'absolute'
+				})
+			}).html('<h2>isian harus lengkap bro</h2>');
+		} else {
+			arrayku.push({
+				'nama' : $('[name="nama"]').val(),
+				'alamat' : $('[name="alamat"]').val(),
+				'pekerjaan' : $('[name="pekerjaan"]').val()
+			});
+			console.log(arrayku);
+			tampildata();
+		}
 	})
 	function tampildata(){
-		$('.hasil ul').empty();
+		$('.hasil table').empty();
+		let no = 1;
 		$.each(arrayku, function(i, val){
-			$('.hasil ul').append('<li>' + val + '</li>')
+			$('.hasil table').append('<tr><td>' + no++ + '</td><td>' + val.nama + '</td><td>' + val.alamat + '</td><td>' + val.pekerjaan + '</td></tr>')
 		})
+		$('[name="nama"]').val('');
+		$('[name="alamat"]').val('');
+		$('[name="pekerjaan"]').val('');
+		$('[name="nama"]').focus();
 	}
 })
